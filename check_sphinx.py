15a546d3e79d8ae6d77dd6b7f4d866e9d0dbd5be
Emmanuel Mathot <emmanuel.mahot@gmail.com>
2014-04-02 08:53:57 +0200

    test non restrictive

--- check_sphinx.py
+++ check_sphinx.py
@@ -4,12 +4,12 @@ import subprocess
 def test_linkcheck(tmpdir):
     doctrees = tmpdir.join("_build/doctrees")
     htmldir = tmpdir.join("_build/html")
-    subprocess.check_call(["sphinx-build", "-blinkcheck", "-d",
+    subprocess.check_call(["sphinx-build", "-W", "-blinkcheck", "-d",
         str(doctrees), "source", str(htmldir)])
 
 
-#def test_build_docs(tmpdir):
-#    doctrees = tmpdir.join("_build/doctrees")
-#    htmldir = tmpdir.join("_build/html")
-#    subprocess.check_call([ "sphinx-build", "-n", "-W", "-bhtml", "-d",
-#        str(doctrees), "source", str(htmldir)])
+def test_build_docs(tmpdir):
+    doctrees = tmpdir.join("_build/doctrees")
+    htmldir = tmpdir.join("_build/html")
+    subprocess.check_call([ "sphinx-build", "-n", "-W", "-bhtml", "-d",
+        str(doctrees), "source", str(htmldir)])
