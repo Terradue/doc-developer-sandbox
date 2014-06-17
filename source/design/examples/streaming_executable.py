c70ac83edbb0d0d041338015e96ea4424f5fa5a3
Fabrice Brito <fabrice.brito@terradue.com>
2014-06-17 11:24:58 +0200

    Update streaming_executable.py

--- source/design/examples/streaming_executable.py
+++ source/design/examples/streaming_executable.py
@@ -3,7 +3,6 @@
 # import modules
 import os
 import sys
-import classes.GDALCalcNDVI as ndvi
 
 # import the cioppy module
 sys.path.append('/usr/lib/ciop/python/')
@@ -26,24 +25,7 @@ for line in sys.stdin:
   
     # do something with the local path
     
-    obj = ndvi.GDALCalcNDVI()
-  
-    obj.calc_ndvi(res[0].rstrip(), '/tmp/result.tif')
-
-    pub = ciop.publish('/tmp/result.tif')
-
-    metadata = [ "ical:dtstart=2001-01-10T14:00:00", 
-                "ical:dtend=2001-01-10T14:05:00",
-                "dc:identifier=mydataset",
-                "dct:spatial=MULTIPOLYGON(((25.55215 36.97701,24.740512 37.091395,24.496927 35.950137,25.284346 35.839142,25.55215 36.97701)))",
-                "dclite4g:onlineResource=" + pub[0].rstrip()]
-    
-    ciop.log('DEBUG', 'Going to register')
-
-    ciop.register('http://localhost/catalogue/sandbox/rdf',
-                    'file:///application/pyjob/etc/series.rdf',
-                    metadata)
-
-    ciop.publish('/tmp/result.tif', metalink = True)
+    # publish
+    pub = ciop.publish(os.environ['TMPDIR'] + '/myresult')
 
 ciop.log('INFO', 'Done my share of the work!')
