d5a8366d139eca7ed769a1fe6576e95ad52f956b
Fabrice Brito <fabrice.brito@terradue.com>
2014-06-17 11:22:57 +0200

    Update streaming_executable.py

--- source/design/examples/streaming_executable.py
+++ source/design/examples/streaming_executable.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python
 
+# import modules
 import os
 import sys
 import classes.GDALCalcNDVI as ndvi
 
+# import the cioppy module
 sys.path.append('/usr/lib/ciop/python/')
-
 import cioppy as ciop
 
-ciop.log('INFO', 'Hello World!')
+myparam1 = ciop.getparam("param1")
+myparam2 = ciop.getparam("param2")
 
-myvar = ciop.getparam('param1')
-
-ciop.log('DEBUG', 'value is: ' + myvar)
+ciop.log("INFO", "I'm running a job with paramaters " + myparam1 + " " + myparam2)
 
 # input comes from STDIN (standard input)
 for line in sys.stdin:
+    
     ciop.log('INFO', 'input: ' + line)
 
+    # copy the input to TMPDIR folder (set as an environmental variable)
     res = ciop.copy(line, os.environ['TMPDIR'])
 
     ciop.log('DEBUG', 'local path:' + res[0].rstrip('\n'))    
   
+    # do something with the local path
+    
     obj = ndvi.GDALCalcNDVI()
   
     obj.calc_ndvi(res[0].rstrip(), '/tmp/result.tif')
